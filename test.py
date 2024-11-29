d3c7024f7463f2840956d5d35e68ca28b4156465
吉村直人 <ahowateto123@gmail.com>
2024-11-29 13:46:53 +0900

    LangChainでOpenAI APIを叩く

---
+++ test.py
@@ -0,0 +1,26 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o", temperature=0)
+
+template = """
+次の文章に誤字がないか調べて。誤字があれば訂正してください。
+{sentences_before_check}
+"""
+
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "あなたは優秀な校正者です。"),
+    ("user", template)
+])
+
+output_parser = StrOutputParser()
+
+chain = prompt | llm | output_parser
+
+answer = chain.invoke({"sentences_before_check": "こんんんちわ、なおとです。"})
+
+print(answer, end="\n")
