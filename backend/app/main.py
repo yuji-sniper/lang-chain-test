f23163164ad8d777374b1028910c16f021fd0a97
吉村直人 <ahowateto123@gmail.com>
2024-12-21 23:52:51 +0900

    main.pyコメントアウト

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from langchain.schema import SystemMessage, HumanMessage
-from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+# from langchain.schema import SystemMessage, HumanMessage
+# from langchain.prompts import ChatPromptTemplate
+# from langchain_openai import ChatOpenAI
+# from langchain_anthropic import ChatAnthropic
 # from langserve import add_routes
 from dotenv import load_dotenv
 
-import multi_actor
+from app import multi_actor
 
 load_dotenv()
 
 app = FastAPI()
 
-openai_model = ChatOpenAI(model="gpt-4o")
-anthropic_model = ChatAnthropic(model_name="claude-3-haiku-20240307")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {"Hello": "World"}
 
-@app.get("/multi_actor")
-def multi_actor_get():
-    return multi_actor.get()
+# openai_model = ChatOpenAI(model="gpt-4o")
+# anthropic_model = ChatAnthropic(model_name="claude-3-haiku-20240307")
 
 # add_routes(
 #     app,
@@ -45,11 +49,3 @@ def multi_actor_get():
 #     joke_prompt | anthropic_model,
 #     path="/joke",
 # )
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
