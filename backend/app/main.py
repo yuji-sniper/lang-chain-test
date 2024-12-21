234592167be00521300e8265ba632ecfe8ec01d5
吉村直人 <ahowateto123@gmail.com>
2024-12-21 19:17:34 +0900

    httpxのバージョン互換性の問題でlangserveを消した

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,8 +4,10 @@ from langchain.schema import SystemMessage, HumanMessage
 from langchain.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-from langserve import add_routes
+# from langserve import add_routes
 from dotenv import load_dotenv
+
+import multi_actor
 
 load_dotenv()
 
@@ -15,30 +17,34 @@ openai_model = ChatOpenAI(model="gpt-4o")
 anthropic_model = ChatAnthropic(model_name="claude-3-haiku-20240307")
 
 @app.get("/")
-def read_root():
+def root():
     return {"Hello": "World"}
 
-add_routes(
-    app,
-    openai_model,
-    path="/openai",
-)
+@app.get("/multi_actor")
+def multi_actor_get():
+    return multi_actor.get()
 
-add_routes(
-    app,
-    anthropic_model,
-    path="/anthropic",
-)
+# add_routes(
+#     app,
+#     openai_model,
+#     path="/openai",
+# )
 
-joke_prompt = ChatPromptTemplate.from_messages([
-    ("system", "ジョークの内容だけを出力してください。前置きは不要です。"),
-    ("user", "{topic}に関するジョークを1つ言ってください。")
-])
-add_routes(
-    app,
-    joke_prompt | anthropic_model,
-    path="/joke",
-)
+# add_routes(
+#     app,
+#     anthropic_model,
+#     path="/anthropic",
+# )
+
+# joke_prompt = ChatPromptTemplate.from_messages([
+#     ("system", "ジョークの内容だけを出力してください。前置きは不要です。"),
+#     ("user", "{topic}に関するジョークを1つ言ってください。")
+# ])
+# add_routes(
+#     app,
+#     joke_prompt | anthropic_model,
+#     path="/joke",
+# )
 
 app.add_middleware(
     CORSMiddleware,
